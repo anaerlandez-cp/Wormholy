e32e2e5a088794d38a153043978f73265992f688
anaerlandez-cp <ana.erlandez@coreproc.com>
2025-08-19 11:02:36 +0800

    wip: add alert to enter passcode

--- Sources/WormholySwift/Subclasses/ShakeDetectionViewController.swift
+++ Sources/WormholySwift/Subclasses/ShakeDetectionViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 extension UIViewController {
     
     open override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        //Shake shake
-        if motion == .motionShake && Wormholy.shakeEnabled {
-            NotificationCenter.default.post(name: fireWormholy, object: nil)
-        }
-        
+		//Shake shake
+		let alert = UIAlertController(title: "Enter Passcode", message: "Please enter the passcode.", preferredStyle: .alert)
+	
+		alert.addTextField { textField in
+			textField.placeholder = "Enter your passcode"
+			textField.keyboardType = .numberPad
+			textField.isSecureTextEntry = true
+		}
+		
+		let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+			if let textField = alert.textFields?.first {
+				let inputText = textField.text ?? ""
+				if inputText == "1" {
+					NotificationCenter.default.post(name: fireWormholy, object: nil)
+				} else {
+					self.showValidationAlert()
+				}
+			}
+		}
+		
+		alert.addAction(okAction)
+		
+		self.present(alert, animated: true, completion: nil)
+		
+		//        if motion == .motionShake && Wormholy.shakeEnabled {
+		//            NotificationCenter.default.post(name: fireWormholy, object: nil)
+		//        }
+		
         next?.motionBegan(motion, with: event)
     }
+	
+	func showValidationAlert() {
+		let alert = UIAlertController(title: "Incorrect passcode!", message: nil, preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "OK", style: .default))
+		present(alert, animated: true)
+	}
 }
